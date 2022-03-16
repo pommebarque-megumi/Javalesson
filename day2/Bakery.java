e9660330cd6876b8379ebd522e3a749e0126e9d8
pommebarque-megumi <cache2gecko@gmail.com>
2022-03-16 14:31:51 +0900

    Bakery追加

---
+++ day2/Bakery.java
@@ -0,0 +1,14 @@
+public class Bakery{
+	public static void main(String[] args){
+int money=0;
+			System.out.println("モーニングセットを注文します");
+		  money=450;
+			System.out.println("total" + money);
+			System.out.println("紅茶はラージサイズに変更します");
+		  money=money+50;
+			System.out.println("total" + money);
+			System.out.println("ヨーグルトの他にサラダを追加します");
+		  money=money+50;
+			System.out.println("total" + money);
+	}
+}
