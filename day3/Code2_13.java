ff0178314cc76369343895ee564ea17398f2e3d7
pommebarque-megumi <cache2gecko@gmail.com>
2022-03-17 12:59:59 +0900

    Integer.parseIntの追加

---
+++ day3/Code2_13.java
@@ -0,0 +1,8 @@
+public class Code2_13{
+	public static void main(String[] args){
+		String age ="31";
+			int n=Integer.parseInt(age);
+			System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+
+	}
+}
