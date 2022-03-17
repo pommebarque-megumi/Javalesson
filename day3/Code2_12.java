b09ab0ac62f717e97cedee1f9e8fd775dee85a2c
pommebarque-megumi <cache2gecko@gmail.com>
2022-03-17 12:39:00 +0900

    math max min

---
+++ day3/Code2_12.java
@@ -0,0 +1,9 @@
+public class Code2_12{
+	public static void main(String[] args){
+		int a = 5;
+		int b = 3;
+		int m =Math.max(a,b);
+		System.out.println("比較実験:" + a +"と" + b +"とで大きいほうは…　" + m);
+		//int m =Math.min(a,b)
+	}
+}
