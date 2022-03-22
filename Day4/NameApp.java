056e2ad99a528a8947ab4d5d315a0ada668c4509
pommebarque-megumi <cache2gecko@gmail.com>
2022-03-22 14:14:18 +0900

    名前の数値化追加

---
+++ Day4/NameApp.java
@@ -0,0 +1,7 @@
+public class NameApp{
+	public static void main(String[] args){
+		int power = '菅' + '原' + 'め' + 'ぐ' + 'み';//足し算することでCharキャラ型から数値に
+		power = power / 5;
+		System.out.println(power);
+	}
+}
